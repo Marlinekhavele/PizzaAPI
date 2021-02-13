e940475873d7c1deec6fd1951449f3a2d4f1797a
MarlineKhavele <marlinekhavele6@gmail.com>
2021-02-13 20:55:25 +0300

    changes

--- base/settings.py
+++ base/settings.py
@@ -128,6 +128,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # for swagger
 
+
+
+
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
