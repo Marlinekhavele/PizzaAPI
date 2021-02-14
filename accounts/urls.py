70c49c97fc606ff83c2e06f217dc230200c41572
MarlineKhavele <marlinekhavele6@gmail.com>
2021-02-14 19:03:14 +0300

    Added staff

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from accounts.views import CustomerViewSet
+from accounts.views import CustomerViewSet,StaffViewSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
+router.register(r'staffs', StaffViewSet, basename='staff')
 router.register(r'customers', CustomerViewSet, basename='customer')
 urlpatterns = router.urls
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
-from accounts.models import Customer
-from accounts.serializers import CustomerSerializer
+from accounts.models import Customer, Staff
+from accounts.serializers import CustomerSerializer,StaffSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+
+class StaffViewSet(viewsets.ModelViewSet):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+    permission_classes = [IsAuthenticated, ]
+
+
+
+    def list(self, request):
+        queryset = Staff.objects.all()
+        serializer = StaffSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Staff.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = StaffSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+        new_staff = StaffSerializer(data=request.data)
+        if new_staff.is_valid():
+            new_staff.save()
+            return Response(new_staff.data, status=201)
+        return Response(new_staff.errors, status=400)
+
+
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     authentication_classes = [BasicAuthentication, ]
-    permission_classes = [IsAuthenticated,IsOwner, IsOwnerStaff ]
+    permission_classes = [IsAuthenticated, ]
 
 
 
