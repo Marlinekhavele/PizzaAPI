71e9d053b217013f3bb64ce2d082e874af9ee025
MarlineKhavele <marlinekhavele6@gmail.com>
2021-02-14 16:47:57 +0300

    static

--- base/settings.py
+++ base/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 REST_FRAMEWORK = {
